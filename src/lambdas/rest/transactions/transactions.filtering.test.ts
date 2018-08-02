24e62b1c269f668b9b64315f57d560e9245e7dd3
tim.j <tim@giftbit.com>
2018-08-02 11:29:33 -0700

    Removed .only

--- src/lambdas/rest/transactions/transactions.filtering.test.ts
+++ src/lambdas/rest/transactions/transactions.filtering.test.ts
@@ -10,7 +10,7 @@ import chaiExclude = require("chai-exclude");
 
 chai.use(chaiExclude);
 
-describe.only("/v2/transactions", () => {
+describe("/v2/transactions", () => {
     const router = new cassava.Router();
 
     before(async function () {
