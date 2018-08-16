1c6cbca1cf18c3f06db2060fd90155eaf85becb7
tim.j <tim@giftbit.com>
2018-08-16 15:28:41 -0700

    removed comments.

--- src/lambdas/postDeploy/schema/V4__issuance_name.sql
+++ src/lambdas/postDeploy/schema/V4__issuance_name.sql
@@ -1,7 +1,6 @@
 ALTER TABLE rothschild.Issuances
   ADD COLUMN name TEXT;
 
-# allow update to not require a primary key in where clause
 SET SQL_SAFE_UPDATES = 0;
 
 UPDATE rothschild.Issuances
@@ -9,7 +8,6 @@ SET
   name = CONCAT('issuance: ', id)
 WHERE NAME IS NULL;
 
-# turn on safe updates again
 SET SQL_SAFE_UPDATES = 1;
 
 ALTER TABLE rothschild.Issuances
