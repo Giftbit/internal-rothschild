548d70d37b08b5678f636d59fe7dc923b2adde9c
tim.j <tim@giftbit.com>
2018-08-16 11:59:29 -0700

    changed sql update syntax. comments on own line.

--- src/lambdas/postDeploy/schema/V4__issuance_name.sql
+++ src/lambdas/postDeploy/schema/V4__issuance_name.sql
@@ -1,14 +1,16 @@
 ALTER TABLE rothschild.Issuances
   ADD COLUMN name TEXT;
 
-SET SQL_SAFE_UPDATES = 0; # allow update to not require a primary key in where clause
+# allow update to not require a primary key in where clause
+SET SQL_SAFE_UPDATES = 0;
 
 UPDATE rothschild.Issuances
 SET
   name = CONCAT('issuance: ', id)
 WHERE NAME IS NULL;
 
-SET SQL_SAFE_UPDATES = 1; # turn on safe updates again
+# turn on safe updates again
+SET SQL_SAFE_UPDATES = 1;
 
 ALTER TABLE rothschild.Issuances
   MODIFY COLUMN name TEXT NOT NULL;
