ea92071fec780cede1153db021d745caf21ab559
tim.j <tim@giftbit.com>
2018-08-16 17:32:28 -0700

    Removed index on userId. It wasn't needed. It was covered by composite primary key.

--- src/lambdas/postDeploy/schema/V5__date_indexes.sql
+++ src/lambdas/postDeploy/schema/V5__date_indexes.sql
@@ -1,19 +1,14 @@
 ALTER TABLE rothschild.Transactions
-  ADD INDEX ix_Transactions_createdDate (createdDate),
-  ADD INDEX ix_Transactions_userId (userId);
+  ADD INDEX ix_Transactions_createdDate (createdDate);
 
 ALTER TABLE rothschild.Values
-  ADD INDEX ix_Values_createdDate (createdDate),
-  ADD INDEX ix_Values_userId (userId);
+  ADD INDEX ix_Values_createdDate (createdDate);
 
 ALTER TABLE rothschild.Contacts
-  ADD INDEX ix_Contacts_createdDate (createdDate),
-  ADD INDEX ix_Contacts_userId (userId);
+  ADD INDEX ix_Contacts_createdDate (createdDate);
 
 ALTER TABLE rothschild.Programs
-  ADD INDEX ix_Programs_createdDate (createdDate),
-  ADD INDEX ix_Programs_userId (userId);
+  ADD INDEX ix_Programs_createdDate (createdDate);
 
 ALTER TABLE rothschild.Issuances
-  ADD INDEX ix_Issuances_createdDate (createdDate),
-  ADD INDEX ix_Issuances_userId (userId);
+  ADD INDEX ix_Issuances_createdDate (createdDate);
