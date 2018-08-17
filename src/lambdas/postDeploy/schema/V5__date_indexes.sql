ac6c0c486f1faca7deda1b036b75c0a5572168db
tim.j <tim@giftbit.com>
2018-08-16 17:07:57 -0700

    Added userId index.

--- src/lambdas/postDeploy/schema/V5__date_indexes.sql
+++ src/lambdas/postDeploy/schema/V5__date_indexes.sql
@@ -1,14 +1,19 @@
 ALTER TABLE rothschild.Transactions
-  ADD INDEX ix_Transactions_createdDate (createdDate);
+  ADD INDEX ix_Transactions_createdDate (createdDate),
+  ADD INDEX ix_Transactions_userId (userId);
 
 ALTER TABLE rothschild.Values
-  ADD INDEX ix_Values_createdDate (createdDate);
+  ADD INDEX ix_Values_createdDate (createdDate),
+  ADD INDEX ix_Values_userId (userId);
 
 ALTER TABLE rothschild.Contacts
-  ADD INDEX ix_Contacts_createdDate (createdDate);
+  ADD INDEX ix_Contacts_createdDate (createdDate),
+  ADD INDEX ix_Contacts_userId (userId);
 
 ALTER TABLE rothschild.Programs
-  ADD INDEX ix_Programs_createdDate (createdDate);
+  ADD INDEX ix_Programs_createdDate (createdDate),
+  ADD INDEX ix_Programs_userId (userId);
 
 ALTER TABLE rothschild.Issuances
-  ADD INDEX ix_Issuances_createdDate (createdDate);
+  ADD INDEX ix_Issuances_createdDate (createdDate),
+  ADD INDEX ix_Issuances_userId (userId);
