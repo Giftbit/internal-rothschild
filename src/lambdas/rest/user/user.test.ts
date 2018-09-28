8ef4f1bf55eb30e1dbb44553303425e28eb6442e
dano <dano@giftbit.com>
2018-09-28 13:50:06 -0700

    Moved secret init and userId Hashing into intercomUtils file

--- src/lambdas/rest/user/user.test.ts
+++ src/lambdas/rest/user/user.test.ts
@@ -4,28 +4,32 @@ import * as chai from "chai";
 import {installRestRoutes} from "../installRestRoutes";
 import chaiExclude = require("chai-exclude");
 import * as crypto from "crypto";
+import {initializeIntercomSecrets} from "../../../utils/intercomUtils";
 
 chai.use(chaiExclude);
 
 describe("/v2/user", () => {
 
+    const intercomTestSecret = "TEST_SECRET";
     const router = new cassava.Router();
 
     before(async () => {
         await testUtils.resetDb();
         router.route(testUtils.authRoute);
         installRestRoutes(router);
+        await initializeIntercomSecrets(Promise.resolve({
+            secretKey: intercomTestSecret
+        }));
     });
 
     describe("/v2/user/intercom", () => {
         it("gets expected hash", async () => {
-            const hmac = crypto.createHmac("sha256", "");
-            hmac.update(testUtils.defaultTestUser.userId);
-            const expectedOutput = hmac.digest("hex");
+            const expectedOutput = crypto.createHmac("sha256", intercomTestSecret)
+                .update(testUtils.defaultTestUser.userId)
+                .digest("hex");
 
             const resp = await testUtils.testAuthedRequest(router, "/v2/user/intercom", "GET");
             chai.assert.equal(resp.statusCode, 200);
-            chai.assert.deepEqual(resp.body, []);
             chai.assert.equal(expectedOutput, resp.body);
         });
     });
--- src/lambdas/rest/user/user.ts
+++ src/lambdas/rest/user/user.ts
@@ -1,12 +1,6 @@
 import * as cassava from "cassava";
 import * as giftbitRoutes from "giftbit-cassava-routes";
-import * as crypto from "crypto";
-
-interface IntercomSecretConfig {
-    secretKey: string;
-}
-
-
+import {hashUserId} from "../../../utils/intercomUtils";
 
 export function installUserRest(router: cassava.Router): void {
     router.route("/v2/user/intercom")
@@ -18,11 +12,8 @@ export function installUserRest(router: cassava.Router): void {
             const auth: giftbitRoutes.jwtauth.AuthorizationBadge = evt.meta["auth"];
             auth.requireIds("userId");
 
-            const hmac = crypto.createHmac("sha256", "");
-            hmac.update(auth.userId);
-
             return {
-                body: hmac.digest("hex")
+                body: hashUserId(auth.userId)
             };
         });
 }
