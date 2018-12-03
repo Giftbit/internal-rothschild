9516136aa9f06a755ae0b314eb674792140ce234
tim.j <tim@giftbit.com>
2018-12-03 10:07:51 -0800

    Starting on performance query.

--- src/lambdas/rest/values.ts
+++ src/lambdas/rest/values.ts
@@ -584,6 +584,163 @@ export async function injectValueStats(auth: giftbitRoutes.jwtauth.Authorization
     }
 }
 
+/**
+ * This is currently a secret operation only the web app knows about.
+ */
+export async function getValuePerformance(auth: giftbitRoutes.jwtauth.AuthorizationBadge, valueId: string): Promise<any> {
+    auth.requireIds("userId");
+
+    const startTime = Date.now();
+    const stats = {
+        // outstanding: { // doesn't seem relevant
+        //     balance: 0,
+        //     count: 0
+        // },
+        // canceled: { // doesn't seem relevant
+        //     balance: 0,
+        //     count: 0
+        // },
+        // expired: { // doesn't seem relevant
+        //     balance: 0,
+        //     count: 0
+        // },
+        // redeemed: { // doesn't seem relevant
+        //     balance: 0,
+        //     count: 0,
+        //     transactionCount: 0
+        // },
+        checkout: {
+            lightrailSpend: 0,
+            overspend: 0,
+            transactionCount: 0
+        },
+        attachedContacts: {
+            count: 0
+        }
+    };
+
+    const now = nowInDbPrecision();
+    const knex = await getKnexRead();
+    const valueStatsRes: { sumBalance: number, count: number, canceled: boolean, expired: boolean }[] = await knex("Values")
+        .where({
+            "userId": auth.userId,
+            "programId": programId,
+            "active": true
+        })
+        .select("canceled")
+        .select(knex.raw("endDate IS NOT NULL AND endDate < ? AS expired", [now]))
+        .sum({sumBalance: "balance"})
+        .count({count: "*"})
+        .groupBy("canceled", "expired");
+    for (const valueStatsResLine of valueStatsRes) {
+        if (valueStatsResLine.canceled) {
+            // Includes canceled AND expired.
+            stats.canceled.balance += +valueStatsResLine.sumBalance;  // for some reason SUM() comes back as a string
+            stats.canceled.count += valueStatsResLine.count;
+        } else if (valueStatsResLine.expired) {
+            stats.expired.balance += +valueStatsResLine.sumBalance;
+            stats.expired.count += valueStatsResLine.count;
+        } else {
+            stats.outstanding.balance += +valueStatsResLine.sumBalance;
+            stats.outstanding.count += valueStatsResLine.count;
+        }
+    }
+
+    log.info(`injectProgramStats got value stats ${Date.now() - startTime}ms`);
+
+    const redeemedStatsRes: { balance: number, transactionCount: number, valueCount: number }[] = await knex("Values")
+        .where({
+            "Values.userId": auth.userId,
+            "Values.programId": programId,
+            "Values.active": true
+        })
+        .join("LightrailTransactionSteps", {
+            "LightrailTransactionSteps.userId": "Values.userId",
+            "LightrailTransactionSteps.valueId": "Values.id"
+        })
+        .where("LightrailTransactionSteps.balanceChange", "<", 0)
+        .sum({balance: "LightrailTransactionSteps.balanceChange"})
+        .countDistinct({transactionCount: "LightrailTransactionSteps.transactionId"})
+        .countDistinct({valueCount: "Values.id"});
+    stats.redeemed.count = redeemedStatsRes[0].valueCount;
+    stats.redeemed.balance = -redeemedStatsRes[0].balance;
+    stats.redeemed.transactionCount = redeemedStatsRes[0].transactionCount;
+
+    log.info(`injectProgramStats got redeemed stats ${Date.now() - startTime}ms`);
+
+    const overspendStatsRes: { lrBalance: number, iBalance: number, sBalance: number, remainder: number, transactionCount: number }[] = await knex
+        .from(knex.raw("? as Txs", [
+            // Get unique Transaction IDs related to the Program
+            knex("Values")
+                .where({
+                    "Values.userId": auth.userId,
+                    "Values.programId": programId,
+                    "Values.active": true
+                })
+                .join("LightrailTransactionSteps", {
+                    "LightrailTransactionSteps.userId": "Values.userId",
+                    "LightrailTransactionSteps.valueId": "Values.id"
+                })
+                .join("Transactions", {
+                    "Transactions.userId": "LightrailTransactionSteps.userId",
+                    "Transactions.id": "LightrailTransactionSteps.transactionId"
+                })
+                .where({"Transactions.transactionType": "checkout"})
+                .distinct("Transactions.id", "Transactions.totals_remainder")
+        ]))
+        .leftJoin(
+            // For each Transaction, sum LightrailTransactionSteps.balanceChange
+            knex.raw(
+                "? as LightrailBalances on LightrailBalances.transactionId = Txs.id",
+                [
+                    knex("LightrailTransactionSteps")
+                        .where({userId: auth.userId})
+                        .groupBy("transactionId")
+                        .sum("balanceChange as balanceChange")
+                        .select("transactionId")
+                ]
+            )
+        )
+        .leftJoin(
+            // For each Transaction, sum InternalTransactionSteps.balanceChange
+            knex.raw(
+                "? as InternalBalances on InternalBalances.transactionId = Txs.id",
+                [
+                    knex("InternalTransactionSteps")
+                        .where({userId: auth.userId})
+                        .groupBy("transactionId")
+                        .sum("balanceChange as balanceChange")
+                        .select("transactionId")
+                ]
+            )
+        )
+        .leftJoin(
+            // For each Transaction, sum StripeTransactionSteps.amount
+            knex.raw(
+                "? as StripeAmounts on StripeAmounts.transactionId = Txs.id",
+                [
+                    knex("StripeTransactionSteps")
+                        .where({userId: auth.userId})
+                        .groupBy("transactionId")
+                        .sum("amount as amount")
+                        .select("transactionId")
+                ]
+            )
+        )
+        .countDistinct({transactionCount: "Txs.id"})
+        .sum({remainder: "Txs.totals_remainder"})
+        .sum({lrBalance: "LightrailBalances.balanceChange"})
+        .sum({iBalance: "InternalBalances.balanceChange"})
+        .sum({sBalance: "StripeAmounts.amount"});
+    stats.checkout.transactionCount = overspendStatsRes[0].transactionCount;
+    stats.checkout.lightrailSpend = -overspendStatsRes[0].lrBalance;
+    stats.checkout.overspend = -overspendStatsRes[0].iBalance - overspendStatsRes[0].sBalance + +overspendStatsRes[0].remainder;
+
+    log.info(`injectProgramStats got overspend stats and done ${Date.now() - startTime}ms`);
+
+    return stats;
+}
+
 function initializeValue(auth: giftbitRoutes.jwtauth.AuthorizationBadge, partialValue: Partial<Value>, program: Program = null, generateCodeParameters: GenerateCodeParameters = null): Value {
     const now = nowInDbPrecision();
 
