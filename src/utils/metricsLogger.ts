850e9e209713f0549d01ebc50dc1dda8dce9ccfa
Tana Jukes <tana.j@lightrail.com>
2019-01-07 14:21:11 -0800

    Constrain metrics types

--- src/utils/metricsLogger.ts
+++ src/utils/metricsLogger.ts
@@ -6,10 +6,9 @@ import log = require("loglevel");
  * Log message follows format `MONITORING|<unix_epoch_timestamp_in_seconds>|<value>|<metric_type>|<metric_name>|#<tag_key>:<tag_value>`
  * The tag function_name:<name_of_the_function> is added automatically
  */
-
-export function logMetric(value: number, metricType: string /*must be one of count, histogram, etc*/, metricName: string /*this is fragile*/, tags: {} | { [key: string]: string }, auth: giftbitRoutes.jwtauth.AuthorizationBadge): void {
+export function logMetric(value: number, metricType: metricsType, metricName: string /*this is fragile*/, tags: {} | { [key: string]: string }, auth: giftbitRoutes.jwtauth.AuthorizationBadge): void {
     let tagString: string = "";
-    Object.keys(tags).forEach(key => tagString += `#${key}:${tags[key]},`); // trailing comma: these tags are inserted first below
+    Object.keys(tags).forEach(key => tagString += `#${key}:${tags[key]},`);
 
     log.info(`MONITORING|` +
         `${Math.round(Date.now() / 1000)}|` +
@@ -21,3 +20,12 @@ export function logMetric(value: number, metricType: string /*must be one of cou
         `#teamMemberId:${auth.teamMemberId}`);
 }
 
+/**
+ * Legal types of metrics: https://docs.datadoghq.com/integrations/amazon_lambda/#using-cloudwatch-logs
+ */
+export enum metricsType {
+    histogram = "histogram",
+    count = "count",
+    gauge = "gauge",
+    check = "check"
+}
