fc06aa0071121598c8e9ec0ab45409b42468a9fc
tim.j <tim@giftbit.com>
2019-01-10 16:03:12 -0800

    Includes the 2 bug fixes and increases timeout for stats test. it timed out during stripeLive.

--- src/lambdas/rest/programs.test.ts
+++ src/lambdas/rest/programs.test.ts
@@ -1648,4 +1648,48 @@ describe("/v2/programs", () => {
         const create = await testUtils.testAuthedRequest<cassava.RestError>(router, "/v2/programs", "POST", program);
         chai.assert.equal(create.statusCode, 201);
     });
+
+    it("can't create a program with duplicate fixedInitialUsesRemaining", async () => {
+        const program: Partial<Program> = {
+            id: generateId(),
+            name: "name " + generateId(5),
+            currency: "USD",
+            fixedInitialUsesRemaining: [1, 1, 2]
+        };
+        const create = await testUtils.testAuthedRequest<cassava.RestError>(router, "/v2/programs", "POST", program);
+        chai.assert.equal(create.statusCode, 422);
+    });
+
+    it("can create a program with distinct fixedInitialUsesRemaining", async () => {
+        const program: Partial<Program> = {
+            id: generateId(),
+            name: "name " + generateId(5),
+            currency: "USD",
+            fixedInitialUsesRemaining: [1, 2, 3]
+        };
+        const create = await testUtils.testAuthedRequest<cassava.RestError>(router, "/v2/programs", "POST", program);
+        chai.assert.equal(create.statusCode, 201);
+    });
+
+    it("can't create a program with duplicate fixedInitialBalances", async () => {
+        const program: Partial<Program> = {
+            id: generateId(),
+            name: "name " + generateId(5),
+            currency: "USD",
+            fixedInitialBalances: [1, 1, 2]
+        };
+        const create = await testUtils.testAuthedRequest<cassava.RestError>(router, "/v2/programs", "POST", program);
+        chai.assert.equal(create.statusCode, 422);
+    });
+
+    it("can create a program with destinct fixedInitialBalances", async () => {
+        const program: Partial<Program> = {
+            id: generateId(),
+            name: "name " + generateId(5),
+            currency: "USD",
+            fixedInitialBalances: [1, 2, 3]
+        };
+        const create = await testUtils.testAuthedRequest<cassava.RestError>(router, "/v2/programs", "POST", program);
+        chai.assert.equal(create.statusCode, 422);
+    });
 });
