3bb9a1bc8dd19bb1bb3ba5fa2f02911697b7aa73
Tana Jukes <tana.j@lightrail.com>
2019-03-05 18:19:47 -0800

    WIP util methods & stub

--- src/lambdas/stripeEventWebhook/installStripeEventWebhookRoute.ts
+++ src/lambdas/stripeEventWebhook/installStripeEventWebhookRoute.ts
@@ -1,9 +1,11 @@
 import log = require("loglevel");
 import Stripe = require("stripe");
+import * as stripe from "stripe";
 import * as cassava from "cassava";
 import {getLightrailStripeModeConfig} from "../../utils/stripeUtils/stripeAccess";
 import {StripeModeConfig} from "../../utils/stripeUtils/StripeConfig";
 import * as giftbitRoutes from "giftbit-cassava-routes";
+import {AuthorizationBadge} from "giftbit-cassava-routes/dist/jwtauth";
 
 
 export function installStripeEventWebhookRoute(router: cassava.Router): void {
@@ -33,4 +35,38 @@ export function installStripeEventWebhookRoute(router: cassava.Router): void {
                 body: null
             };
         });
+}
+
+async function handleRefundForFraud(event: stripe.events.IEvent & { account: string }): Promise<void> {
+}
+
+/**
+ * This is a workaround method. When we can get the Lightrail userId directly from the Stripe accountId, we won't need to pass in the charge.
+ * @param stripeAccountId
+ * @param stripeCharge
+ */
+function getAuthBadgeFromStripeCharge(stripeAccountId: string, stripeCharge: stripe.charges.ICharge): giftbitRoutes.jwtauth.AuthorizationBadge {
+    const lightrailUserId = getLightrailUserIdFromStripeCharge(stripeAccountId, stripeCharge);
+
+    return new AuthorizationBadge({
+        g: {
+            gui: lightrailUserId,
+            tmi: lightrailUserId,
+        }
+    });
+}
+
+/**
+ * This is a workaround method. For now, it relies on finding the Lightrail userId directly in the charge metadata.
+ * This is not reliable or safe as a permanent solution; it's waiting on the new user service to provide a direct mapping
+ * from Stripe accountId to Lightrail userId. When that happens, we won't need to pass the charge object in.
+ * @param stripeAccountId
+ * @param stripeCharge
+ */
+function getLightrailUserIdFromStripeCharge(stripeAccountId: string, stripeCharge: stripe.charges.ICharge): string {
+    if (stripeCharge.metadata["lightrailUserId"] && stripeCharge.metadata["lightrailUserId"].length > 0) {
+        return stripeCharge.metadata["lightrailUserId"];
+    } else {
+        throw new Error(`Could not get Lightrail userId from Stripe accountId ${stripeAccountId} and charge ${stripeCharge.id}`);
+    }
 }
