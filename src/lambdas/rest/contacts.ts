54244e6f0f3f95510084c5da113adb8490f70489
arshdeepvirk <arshvirk777@gmail.com>
2019-10-11 13:24:09 -0700

    work in progress

--- src/lambdas/rest/contacts.ts
+++ src/lambdas/rest/contacts.ts
@@ -193,6 +193,7 @@ export async function createContact(auth: giftbitRoutes.jwtauth.AuthorizationBad
         return contact;
     } catch (err) {
         if (err.code === "ER_DUP_ENTRY") {
+            console.log("herererre");
             throw new cassava.RestError(cassava.httpStatusCode.clientError.CONFLICT, `Contact with id '${contact.id}' already exists.`);
         }
         throw err;
