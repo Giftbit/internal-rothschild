517700556d6d41ed31292497084265cc3a099d07
tim.j <tim@giftbit.com>
2019-11-06 15:49:47 -0800

    Added program migration.

--- src/lambdas/postDeploy/schema/V30__discountSellerLiabilityRule_explanation_udpate.sql
+++ src/lambdas/postDeploy/schema/V30__discountSellerLiabilityRule_explanation_udpate.sql
@@ -18,4 +18,22 @@ SET discountSellerLiabilityRule = CONCAT('{"rule":"',
 WHERE discountSellerLiabilityRule IS NOT NULL
   AND discountSellerLiabilityRule LIKE '%Populated from deprecated property discountSellerLiability.%';
 
+UPDATE rothschild.`Programs`
+SET discountSellerLiabilityRule = CONCAT('{"rule":"',
+                                         CAST(SUBSTR(discountSellerLiabilityRule,
+                                                     LOCATE('"rule":"', discountSellerLiabilityRule) + 8,
+                                                     (LOCATE('",', discountSellerLiabilityRule) -
+                                                      (LOCATE('"rule":"', discountSellerLiabilityRule) + 8)))
+                                              AS CHAR),
+                                         CONCAT('", "explanation":"Seller ',
+                                                CAST(SUBSTR(discountSellerLiabilityRule,
+                                                            LOCATE('"rule":"', discountSellerLiabilityRule) + 8,
+                                                            (LOCATE('",', discountSellerLiabilityRule) -
+                                                             (LOCATE('"rule":"', discountSellerLiabilityRule) + 8))) *
+                                                     100
+                                                     AS CHAR),
+                                                '% liable"}'))
+WHERE discountSellerLiabilityRule IS NOT NULL
+  AND discountSellerLiabilityRule LIKE '%Populated from deprecated property discountSellerLiability.%';
+
 SET SQL_SAFE_UPDATES = 1;
