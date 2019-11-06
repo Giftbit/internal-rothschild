0c452d2a55aec43bb066fe12d9523f044d8da35b
tim.j <tim@giftbit.com>
2019-11-06 12:12:02 -0800

    Updated migration query.

--- src/lambdas/postDeploy/schema/V30__discountSellerLiabilityRule_explanation_udpate.sql
+++ src/lambdas/postDeploy/schema/V30__discountSellerLiabilityRule_explanation_udpate.sql
@@ -3,25 +3,19 @@ SET SQL_SAFE_UPDATES = 0;
 UPDATE rothschild.`Values`
 SET discountSellerLiabilityRule = CONCAT('{"rule":"',
                                          CAST(SUBSTR(discountSellerLiabilityRule,
-                                                     LOCATE('"rule":', discountSellerLiabilityRule) + 8,
-                                                     LOCATE('"explanation":',
-                                                            discountSellerLiabilityRule) -
-                                                     (LOCATE('"rule":', discountSellerLiabilityRule) + 10))
+                                                     LOCATE('"rule":"', discountSellerLiabilityRule) + 8,
+                                                     (LOCATE('",', discountSellerLiabilityRule) -
+                                                      (LOCATE('"rule":"', discountSellerLiabilityRule) + 8)))
                                               AS CHAR),
                                          CONCAT('", "explanation":"Seller ',
                                                 CAST(SUBSTR(discountSellerLiabilityRule,
-                                                            LOCATE('"rule":', discountSellerLiabilityRule) + 8,
-                                                            LOCATE('"explanation":',
-                                                                   discountSellerLiabilityRule) -
-                                                            (LOCATE('"rule":', discountSellerLiabilityRule) + 10)) * 100
+                                                            LOCATE('"rule":"', discountSellerLiabilityRule) + 8,
+                                                            (LOCATE('",', discountSellerLiabilityRule) -
+                                                             (LOCATE('"rule":"', discountSellerLiabilityRule) + 8))) *
+                                                     100
                                                      AS CHAR),
                                                 '% liable"}'))
 WHERE discountSellerLiabilityRule IS NOT NULL
-  AND SUBSTR(discountSellerLiabilityRule,
-             LOCATE('"explanation":',
-                    discountSellerLiabilityRule) + 15,
-             LOCATE('}', discountSellerLiabilityRule) - (LOCATE('"explanation":',
-                                                                discountSellerLiabilityRule) + 16)) =
-      'Populated from deprecated property discountSellerLiability.';
+  AND discountSellerLiabilityRule REGEXP '"Populated from deprecated property discountSellerLiability."'
 
 SET SQL_SAFE_UPDATES = 1;
