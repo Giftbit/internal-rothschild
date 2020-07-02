3d24355a4b8d5a334771a0b973ba27062d6b851e
timjordison <tim.j@lightrail.com>
2020-07-02 16:42:15 -0700

    fix attachGenericAsNewValue tests

--- src/lambdas/rest/contactValues.attachNewValue.test.ts
+++ src/lambdas/rest/contactValues.attachNewValue.test.ts
@@ -13,7 +13,7 @@ import {generateUrlSafeHashFromValueIdContactId} from "./genericCodeWithPerConta
 import {CheckoutRequest} from "../../model/TransactionRequest";
 import {nowInDbPrecision} from "../../utils/dbUtils";
 
-describe("/v2/contacts/values - attachNewValue=true", () => {
+describe.only("/v2/contacts/values - attachNewValue=true", () => {
 
     const router = new cassava.Router();
 
@@ -182,8 +182,7 @@ describe("/v2/contacts/values - attachNewValue=true", () => {
                 explanation: "$5 done the hard way"
             },
             code: generateFullcode(),
-            isGenericCode: true,
-            usesRemaining: 20
+            isGenericCode: true
         };
 
         const createValueResp = await testUtils.testAuthedRequest<Value>(router, "/v2/values", "POST", value);
@@ -205,11 +204,6 @@ describe("/v2/contacts/values - attachNewValue=true", () => {
         chai.assert.isNotNull(attachResp.body.updatedContactIdDate);
         chai.assert.equal(attachResp.body.updatedContactIdDate, attachResp.body.updatedDate);
 
-        // usesRemaining should be decremented on original Value.
-        const getValueResp = await await testUtils.testAuthedRequest<Value>(router, `/v2/values/${createValueResp.body.id}`, "GET");
-        chai.assert.equal(attachResp.statusCode, 200, `body=${JSON.stringify(getValueResp.body)}`);
-        chai.assert.equal(getValueResp.body.usesRemaining, createValueResp.body.usesRemaining - 1);
-
         // Should be a transaction for the attach.
         const getTxResp = await testUtils.testAuthedRequest<Transaction>(router, `/v2/transactions/${encodeURIComponent(attachResp.body.id)}`, "GET");
         chai.assert.equal(getTxResp.statusCode, 200, `there should be a transaction for the attach body=${JSON.stringify(attachResp.body)}`);
@@ -226,9 +220,9 @@ describe("/v2/contacts/values - attachNewValue=true", () => {
                     balanceBefore: null,
                     balanceAfter: null,
                     balanceChange: 0,
-                    usesRemainingBefore: createValueResp.body.usesRemaining,
-                    usesRemainingAfter: createValueResp.body.usesRemaining - 1,
-                    usesRemainingChange: -1
+                    usesRemainingBefore: null,
+                    usesRemainingAfter: null,
+                    usesRemainingChange: null
                 },
                 {
                     rail: "lightrail",
@@ -238,85 +232,6 @@ describe("/v2/contacts/values - attachNewValue=true", () => {
                     balanceBefore: null,
                     balanceAfter: null,
                     balanceChange: 0,
-                    usesRemainingBefore: 0,
-                    usesRemainingAfter: 1,
-                    usesRemainingChange: 1
-                }
-            ],
-            totals: null,
-            lineItems: null,
-            paymentSources: null,
-            pending: false,
-            createdDate: attachResp.body.createdDate,
-            createdBy: attachResp.body.createdBy,
-            metadata: null,
-            tax: null
-        });
-    });
-
-    it("can attach a generic-code Value with a balance using attachNewValue=true to create a new Value", async () => {
-        const value: Partial<Value> = {
-            id: generateId(),
-            currency: currency.code,
-            balance: 500,
-            code: generateFullcode(),
-            isGenericCode: true,
-            usesRemaining: 135
-        };
-
-        const createValueResp = await testUtils.testAuthedRequest<Value>(router, "/v2/values", "POST", value);
-        chai.assert.equal(createValueResp.statusCode, 201, `body=${JSON.stringify(createValueResp.body)}`);
-
-        // Should return a new Value.
-        const attachResp = await testUtils.testAuthedRequest<Value>(router, `/v2/contacts/${contact.id}/values/attach`, "POST", {
-            code: value.code,
-            attachGenericAsNewValue: true
-        });
-        chai.assert.equal(attachResp.statusCode, 200, `body=${JSON.stringify(attachResp.body)}`);
-        chai.assert.equal(attachResp.body.currency, createValueResp.body.currency);
-        chai.assert.deepEqual(attachResp.body.balance, createValueResp.body.balance);
-        chai.assert.equal(attachResp.body.contactId, contact.id);
-        chai.assert.equal(attachResp.body.usesRemaining, 1);
-        chai.assert.equal(attachResp.body.code, null);
-        chai.assert.equal(attachResp.body.isGenericCode, false);
-        chai.assert.notEqual(attachResp.body.id, createValueResp.body.id);
-        chai.assert.isNotNull(attachResp.body.updatedContactIdDate);
-        chai.assert.equal(attachResp.body.updatedContactIdDate, attachResp.body.updatedDate);
-
-        // usesRemaining should be decremented on original Value and balance unchanged.
-        const getValueResp = await await testUtils.testAuthedRequest<Value>(router, `/v2/values/${createValueResp.body.id}`, "GET");
-        chai.assert.equal(attachResp.statusCode, 200, `body=${JSON.stringify(getValueResp.body)}`);
-        chai.assert.equal(getValueResp.body.balance, createValueResp.body.balance);
-        chai.assert.equal(getValueResp.body.usesRemaining, createValueResp.body.usesRemaining - 1);
-
-        // Should be a transaction for the attach.
-        const getTxResp = await testUtils.testAuthedRequest<Transaction>(router, `/v2/transactions/${encodeURIComponent(attachResp.body.id)}`, "GET");
-        chai.assert.equal(getTxResp.statusCode, 200, `there should be a transaction for the attach body=${JSON.stringify(attachResp.body)}`);
-        chai.assert.deepEqual(getTxResp.body, {
-            id: attachResp.body.id,
-            transactionType: "attach",
-            currency: attachResp.body.currency,
-            steps: [
-                {
-                    rail: "lightrail",
-                    valueId: createValueResp.body.id,
-                    contactId: null,
-                    code: null,
-                    balanceBefore: createValueResp.body.balance,
-                    balanceAfter: createValueResp.body.balance,
-                    balanceChange: 0,
-                    usesRemainingBefore: createValueResp.body.usesRemaining,
-                    usesRemainingAfter: createValueResp.body.usesRemaining - 1,
-                    usesRemainingChange: -1
-                },
-                {
-                    rail: "lightrail",
-                    valueId: attachResp.body.id,
-                    contactId: attachResp.body.contactId,
-                    code: null,
-                    balanceBefore: 0,
-                    balanceAfter: createValueResp.body.balance,
-                    balanceChange: createValueResp.body.balance,
                     usesRemainingBefore: 0,
                     usesRemainingAfter: 1,
                     usesRemainingChange: 1
@@ -338,9 +253,11 @@ describe("/v2/contacts/values - attachNewValue=true", () => {
             id: generateId(),
             currency: currency.code,
             code: generateFullcode(),
-            balance: 500,
-            isGenericCode: true,
-            usesRemaining: 135
+            balanceRule: {
+                rule: "500 + value.balanceChange",
+                explanation: "$5"
+            },
+            isGenericCode: true
         };
 
         const createValueResp = await testUtils.testAuthedRequest<Value>(router, "/v2/values", "POST", value);
@@ -369,8 +286,10 @@ describe("/v2/contacts/values - attachNewValue=true", () => {
             isGenericCode: true,
             code: generateFullcode(),
             discount: true,
-            balance: 400,
-            usesRemaining: 4 // can be attached 4 times
+            balanceRule: {
+                rule: "400 + value.balanceChange",
+                explanation: "$4"
+            }
         };
 
         before(async function () {
@@ -382,19 +301,7 @@ describe("/v2/contacts/values - attachNewValue=true", () => {
             chai.assert.deepNestedInclude(create.body, genericValue);
         });
 
-        it("stats don't include debits on the generic code itself", async () => {
-            // debit balance
-            const debit = await testUtils.testAuthedRequest<Transaction>(router, "/v2/transactions/debit", "POST", {
-                id: "debit-1",
-                source: {
-                    rail: "lightrail",
-                    valueId: genericValue.id
-                },
-                uses: 2, // effectively reduces attaches remaining by 2
-                currency: "USD"
-            });
-            chai.assert.equal(debit.statusCode, 201);
-
+        it("can get stats", async () => {
             // attach to contact as new Value
             const attachResp1 = await testUtils.testAuthedRequest<Value>(router, `/v2/contacts/${contactForStatsTest}/values/attach`, "POST", {
                 code: genericValue.code,
